be8cf93501a9cc9ebd6f45ed81df51ca76d94de5
Eoyzyc <47476221+eoyzyc@users.noreply.github.com>
2021-06-09 10:51:14 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,24 +1,24 @@
 document.addEventListener('keydown', function() {
   if (event.keyCode == 123) {
-    alert("This function has been disabled to prevent you from stealing my code!");
+    alert("Haha u cant see my code noob");
     return false;
   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    alert("This function has been disabled to prevent you from stealing my code!");
+    alert("Haha u cant see my code noob");
     return false;
   } else if (event.ctrlKey && event.keyCode == 85) {
-    alert("This function has been disabled to prevent you from stealing my code!");
+    alert("Haha u cant see my code noob");
     return false;
   }
 }, false);
 
 if (document.addEventListener) {
   document.addEventListener('contextmenu', function(e) {
-    alert("This function has been disabled to prevent you from stealing my code!");
+    alert("Haha u cant see my code noob");
     e.preventDefault();
   }, false);
 } else {
   document.attachEvent('oncontextmenu', function() {
-    alert("This function has been disabled to prevent you from stealing my code!");
+    alert("Haha u cant see my code noob");
     window.event.returnValue = false;
   });
 }
